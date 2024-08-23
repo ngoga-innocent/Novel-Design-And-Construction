5162b09ea4ac5e19d59deda8b3cdd265b9648f88
ngoga-innocent <ngogainnocent1@gmail.com>
2024-08-23 17:44:06 +0200

    size fixing

--- NDC/settings.py
+++ NDC/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cdh^fimo(hyib@g3*4k8rk$5gnp6g12ht#i0kx%cbi3pdzc0+*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
